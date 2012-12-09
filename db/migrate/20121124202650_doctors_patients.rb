2f0a812a87ec448dafb109d59c20871935110bb7
hsichang <hsi.chang@gmail.com>
2012-12-09 16:07:44 -0500

    Doctors can now link with patients

--- db/migrate/20121124202650_doctors_patients.rb
+++
@@ -1,8 +0,0 @@
-class DoctorsPatients < ActiveRecord::Migration
-  def change
-    create_table :doctors_patients do |t|
-      t.integer :patient_id
-      t.integer :doctor_id
-    end
-  end
-end
